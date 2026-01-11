61bfebf0485b97b33bd57ca1129811c2e2a8a5d7
vishwas726 <vishwaslandge2004@gmail.com>
2026-01-11 13:23:31 +0530

    first commit

---
+++ left-pyramid.js
@@ -0,0 +1,28 @@
+n=10
+
+for(let i=0;i<n;i++){
+  let line="";
+
+  for(let j=0;j<i;j++){
+      
+    line+="*";
+
+  }
+
+  console.log(line);
+
+}
+
+for(let i=0;i<=n;i++){
+  
+  line="";
+
+  for(let j=1;j<=i;j++){
+   line+=j;
+  }
+
+  console.log(line)
+  
+}
+
+
---
+++ right-pyramid.js
@@ -0,0 +1,15 @@
+n=6
+
+for(let i=0;i<=n;i++){
+  line="";
+  
+  for(let j=n;j>=i;j--){
+    line+=" "
+  }
+  for(let j=0;j<=i;j++){
+     line+="*"
+  }
+
+  console.log(line)
+
+}
